6bd055abbc576ab32dbba64b3b7dd44b03545914
Elzbieta Wilk <121565930+ElzbietaZofia@users.noreply.github.com>
2023-03-16 16:56:43 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
 
-    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
